a24e0a4694da49ddc6e578323ae0c23665e9eff6
Anass Zahraoui <anouchzahraoui@gmail.com>
2024-04-02 13:56:39 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res) => {
+    res.send("Hello Node Api")
+})
+
+app.listen(3000, () => {
+    console.log("Node api is runnung in port 3000")
+})
